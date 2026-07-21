b5ea7715c13d213d60c5fec45937f795e2eb6453
Navoda Perera <e23271@eng.pdn.ac.lk>
2026-07-21 11:15:31 +0530

    Create users.js with user data
    Added an array of user objects with credentials and roles.

---
+++ Codes/Js/users.js
@@ -0,0 +1,35 @@
+const users = [
+
+    {
+        username: "admin",
+        password: "admin123",
+        name: "System Administrator",
+        role: "ADMIN"
+    },
+
+
+    {
+        username: "jmo001",
+        password: "jmo123",
+        name: "Dr. Silva",
+        role: "JMO"
+    },
+
+
+    {
+        username: "doc001",
+        password: "doc123",
+        name: "Dr. Perera",
+        role: "DOCTOR"
+    },
+
+
+    {
+        username: "clerk001",
+        password: "clerk123",
+        name: "Amal",
+        role: "CLERK"
+    }
+
+
+];
